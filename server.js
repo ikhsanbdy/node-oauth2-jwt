092685d48f42f1f938f4c7597e9d895157a810be
wawando <muhammad.ridwan.ab@gmail.com>
2017-10-23 15:36:31 +0700

    Allowed PATCH method in response headers

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(bodyParser.json());
 // CORS
 app.use(function(req, res, next) {
 	res.setHeader("Access-Control-Allow-Origin", "*");
-	res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+	res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
 	res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
 	
 	return (req.method === "OPTIONS") ? res.end() : next();
